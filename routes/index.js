a3bd44b40249a539b7d53c9c4ee2b04f386ec7a8
Dan Hart <danhart@notonthehighstreet.com>
2014-06-03 22:57:18 +0100

    Extract a PageBuilder class from the router

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var nothsLayout = require('../lib/noths-layout');
-
-var renderResponse = function(view, locals, req, res, next) {
-    res.render(view, locals, function(err, html) {
-        if (err) {
-            next(err);
-            return;
-        }
-
-        res.send(nothsLayout.layout);
-    });
-};
+var PageBuilder = require('../lib/page-builder');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    renderResponse('index', { title: 'test' }, req, res, next);
+    var pageBuilder = new PageBuilder('index', res, next);
+
+    pageBuilder.build(function(html) {
+        res.send(html);
+    });
 });
 
 module.exports = router;
