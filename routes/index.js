436ae716606ada4cff49ec1497c1a6f877fa4a81
Dan Hart <danhart@notonthehighstreet.com>
2014-06-19 12:17:22 +0100

    Turn index into an example route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 var layoutService = require('../services/layout-service');
 var PageBuilder = require('page-builder');
 
-router.get('/', function(req, res, next) {
-    new PageBuilder('index', layoutService, res, next).render();
+// Example route
+router.get('/test', function(req, res, next) {
+    // TODO: Extract these to view helpers
+    var js = '<script>require(["/static-pages-assets/scripts/test.js"]);</script>';
+    var styles = '<link rel="stylesheet" href="/static-pages-assets/stylesheets/test.css"/>';
+
+    new PageBuilder('test', layoutService, res, next).
+        setJavascript(js).
+        setStyles(styles).
+        render();
 });
 
 module.exports = router;
